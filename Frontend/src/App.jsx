d6da78c52b3917107caae6a5dc95fcd2996c1d79
Aman Wairagkar <124350777+Amanw-25@users.noreply.github.com>
2025-01-22 18:23:03 +0530

    Directory Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-
-export default function App() {
+import "./App.css"
+import { Layout } from "./layout/Layout"
+function App() {
   return (
-    <Button 
-      variant="contained"
-      sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)', 
-      }}
-    >
-      Hello World
-    </Button>
-  );
+    <Layout />
+  )
 }
+
+export default App
