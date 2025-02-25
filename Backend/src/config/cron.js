1b7b9bb9869e2c6e419a58c6ca5259ddb2dbdbcf
amanw-25 <amanwairagkar@gmail.com>
2025-02-25 23:49:04 +0530

    Deploy

--- Backend/src/config/cron.js
+++ Backend/src/config/cron.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { assignBadgesAutomatically } from '../utils/badgeService.js';
 
-cron.schedule('45 23 * * *', async () => {
+cron.schedule('55 23 * * *', async () => {
   console.log('Running badge assignment...');
   await assignBadgesAutomatically();
   console.log("Badge assignment complete");  
