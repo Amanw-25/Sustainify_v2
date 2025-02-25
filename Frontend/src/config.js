41f2509de6962560324be31bcf47333037b3746d
amanw-25 <amanwairagkar@gmail.com>
2025-02-25 18:06:12 +0530

    Deploy

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL="http://localhost:5130/api/v1/sustainify";
+// export const BASE_URL="http://localhost:5130/api/v1/sustainify";
+export const BASE_URL="https://sustainify-v2.onrender.com/api/v1/sustainify";
+
 
